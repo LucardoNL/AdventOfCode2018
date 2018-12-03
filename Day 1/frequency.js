99c43ad4ed65e7e111e1c5a6c60ed04fb5c2a35c
Luuk Leeuwenstein <luukleeuwenstein@gmail.com>
2018-12-03 18:58:53 +0100

    Refactored getinput func

--- Day 1/frequency.js
+++ Day 1/frequency.js
@@ -1,9 +1,13 @@
 
-function getinput() {
-    var firstinput = document.getElementById("input").value.split(" ").map(Number)
-    console.log(firstinput)
-    return firstinput
-}
+// function getinput() {
+//     var firstinput = document.getElementById("input").value.split(" ").map(Number)
+//     console.log(firstinput)
+//     return firstinput
+// }
+
+var getinput = () => document.getElementById("input").value
+    .split(" ")
+    .map(Number)
 
 function calculate() {
     getinputs = getinput()
