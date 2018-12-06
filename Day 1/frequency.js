0fbea6087e1567fb256e40284eb99526f81443b0
Luuk Leeuwenstein <Luuk Leeuwenstein>
2018-12-06 19:54:37 +0100

    Finished day 1

--- Day 1/frequency.js
+++ Day 1/frequency.js
@@ -8,23 +8,28 @@ var calculate = () => {
     setresult (reduced)
 }
 
+// pattcalculate will repeat the frequency list untill a double value is found.
 var pattcalculate = () => {
     let frequency = 0
     let pastfrequencies = []
     let found = 0
-    //while (found == 0) {
-        for (var value of getinput()) {
+    let getinputs = getinput()
+    let round = 200
+    while (found < round ) {
+        for (var value of getinputs) {
             frequency += value
             if (pastfrequencies.indexOf(frequency) > -1) {
-                setresult(frequency)
-                found = 1
+                setresult("Found " + frequency)
+                found = round
+                break // Required to stop the search if found in middle of list
             }
             else {
                 pastfrequencies.push(frequency)
-                setresult("Not yet found")
+                setresult("Not yet found " + frequency)
             }
         }
-    //}
+        found += 1
+    }
 }
 
 var setresult = (result) => document.getElementById("output").innerHTML = result
