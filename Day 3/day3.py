36509bf25c901a1504da35bfa519896ece19ebe8
Luuk Leeuwenstein <luukleeuwenstein@gmail.com>
2018-12-17 17:21:42 +0100

    First d3 p1 solution, unsure of outcome and very slow

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,2 +1,24 @@
-parsedline = {'no': 0, 'xstart': 0, 'ystart':0, 'xlenght': 0, 'ylength': 0}
-print(parsedline)
+import re
+
+def mapdata():
+    with open("C:\Box Sync\Luuk Leeuwenstein\LL. Misc\Codings\AdventOfCode2018\Day 3\input.txt") as file:
+        data = file.readlines()
+
+        # Mapping used from answer of u/mserrano on Reddit
+        claims = map(lambda s: map(int, re.findall(r'-?\d+', s)), data)
+        return claims
+
+def patchmapper(claims):
+    patchmap = [{'xy': (int, int)}]
+    match = 0
+    for (id, xstart, ystart, width, height) in claims:
+        for x in range(xstart, xstart+width):
+            for y in range(ystart, ystart+height):
+                if not any(dict['xy'] == (x, y) for dict in patchmap):
+                    patchmap.append({'xy':(x, y)})
+                else:
+                    match += 1
+        print("Claim: "+ str(id) +"\r")               
+    return match
+
+print(patchmapper(mapdata()))
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My attempt at the advent of code 2018.
 ## Status
 Day 1: Part one solved, part two solved.
 Day 2: Part one solved, part two solved.
-Day 3: Not started.
+Day 3: Day 3 started.
